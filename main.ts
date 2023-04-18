a5a2ac268c2882e7e8a1602d26651b63c8c70de5
AmigoDeTomas <130453053+AmigoDeTomas@users.noreply.github.com>
2023-04-18 12:20:38 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     radio.sendString("smile")
 })
 radio.onReceivedString(function (receivedString) {
+    let smile = ""
     basic.showIcon(IconNames.Happy)
+    if (receivedString == smile) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendString("1")
 })
 radio.setGroup(69)
+basic.forever(function () {
+	
+})
 basic.forever(function () {
 	
 })
